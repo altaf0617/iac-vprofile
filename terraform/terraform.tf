5f1576c2953076d15cb6dd3ea2de4030306aa3e9
devop4sure <devop4sure@gmail.com>
2025-11-14 11:35:32 +0500

    test2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,10 +29,10 @@ terraform {
   backend "s3" {
     bucket = "vprofile-actions25"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.9.8"
 }
 ##
 ##
